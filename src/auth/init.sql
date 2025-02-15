6750c7c8594541826b791152a6dbf9d189708216
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-02-15 11:08:56 +0700

    feat(auth-service): implemented basic authentication and jwts revalidate

--- src/auth/init.sql
+++ src/auth/init.sql
@@ -1,15 +1,15 @@
-create user 'sa'@'localhost' identified by 'sa123';
+create user 'auth_user'@'localhost' identified by 'sa123';
 
 create database auth;
 
-grant all PRIVILEGES on auth.* to 'sa'@'localhost';
+grant all PRIVILEGES on auth.* to 'auth_user'@'localhost';
 
 use auth;
 
 create table user(
     id INT not null auto_increment primary key,
-    email varchar(255) not null,
-    password varchar(255) not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null
 );
 
 INSERT INTO user (email, password) VALUES ('sa', 'sa123');
