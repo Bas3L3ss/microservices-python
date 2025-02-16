3c8916b08c1360ccfeca1fde425cb99c2eed3b0b
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-02-16 14:42:34 +0700

    fix(converter-service): fixed modules error, missing modules problem

--- src/converter/convert/to_mp3.py
+++ src/converter/convert/to_mp3.py
@@ -1,42 +1,43 @@
 import pika, json, tempfile, os
-
-import pika.spec
 from bson.objectid import ObjectId
-import moviepy.editor as mp
+import moviepy.editor
+
 
 def start(message, fs_videos, fs_mp3s, channel):
     message = json.loads(message)
 
-    #empty temp file
+    # empty temp file
     tf = tempfile.NamedTemporaryFile()
-    #video contents
+    # video contents
     out = fs_videos.get(ObjectId(message["video_fid"]))
-    #add video contens to empty file
+    # add video contents to empty file
     tf.write(out.read())
     # create audio from temp video file
     audio = moviepy.editor.VideoFileClip(tf.name).audio
     tf.close()
 
-    #write audio to the file 
+    # write audio to the file
     tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
     audio.write_audiofile(tf_path)
 
-    #save the file to mongo
+    # save file to mongo
     f = open(tf_path, "rb")
     data = f.read()
-    fileid = fs_mp3s.put(data)
+    fid = fs_mp3s.put(data)
     f.close()
     os.remove(tf_path)
 
-    message["mp3_fid"] = str(fileid)
-    
+    message["mp3_fid"] = str(fid)
+
     try:
-        channel.basic_publish(exchange='', routing_key=os.environ.get("MP3_QUEUE"), body=json.dumps(message), property=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
-    except Exception as e:
-        fs_mp3s.delete(fileid)
+        channel.basic_publish(
+            exchange="",
+            routing_key=os.environ.get("MP3_QUEUE"),
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ),
+        )
+    except Exception as err:
+        fs_mp3s.delete(fid)
         return "failed to publish message"
-    
-    
-
-     
-
